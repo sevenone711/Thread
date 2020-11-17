69708f7a9e19a55599b223df591df62068f85362
sevenone711 <arbitronin@yandex.ru>
2020-11-17 21:12:58 +0300

    ++

--- src/Main.java
+++ src/Main.java
@@ -32,23 +32,23 @@ public class Main {
         float a2[] = new float[h];
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr,h , a2, 0, h);
-//        Без ожидани
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run () {
-//                for (int i = 0; i < a1.length; i++) {
-//                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-//                }
-//            }
-//        }).start();
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run () {
-//                for (int i = 0; i < a2.length; i++) {
-//                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-//                }
-//            }
-//        }).start();
+        Без ожидани
+        new Thread(new Runnable() {
+            @Override
+            public void run () {
+                for (int i = 0; i < a1.length; i++) {
+                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run () {
+                for (int i = 0; i < a2.length; i++) {
+                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        }).start();
 //        С ожиданием
         var t1 = new Thread(new Runnable() {
             @Override
